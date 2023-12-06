720bdce383cc594aef9268774c80768ba2646778
ethanlanier <115521706+ethanlanier@users.noreply.github.com>
2023-12-06 14:22:41 -0500

    Updated memory item prefab and fixed pause screen

--- Assets/Scripts/MemoryScript.cs
+++ Assets/Scripts/MemoryScript.cs
@@ -2,12 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class MemoryScript : MonoBehaviour
 {
     [SerializeField] PauseScreen pauseScreen;
 
     public Transform container;
     public GameObject memFab;
+
+    
 
     // Start is called before the first frame update
     void Start()
@@ -23,11 +26,15 @@ public class MemoryScript : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
+        MemText pickedUpMemory = other.GetComponent<MemText>();
+
         if (other.gameObject.CompareTag("mem"))
         {
             Destroy(other.gameObject);
             pauseScreen.incrementMem();
-            Instantiate(memFab, container);
+
+            GameObject newUIThing = Instantiate(memFab, container);
+            newUIThing.GetComponent<memoryInfo>().SetMessage(pickedUpMemory.memoryText);
         }
     }
 }
